6cb4c133c6a405b6c76e73787223b98a3adf32b0
Shin Dong Won <eastorigin21@gmail.com>
2024-10-15 18:07:41 +0900

    수정

--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -1,4 +1,7 @@
 $().ready(function () {
+  var isIdChecked = false;
+  var isEmailChecked = false;
+
   $("#checkId").on("click", function () {
     var id = $("#id").val();
     $.ajax({
@@ -8,6 +11,7 @@ $().ready(function () {
       success: function (response) {
         if (response.available) {
           alert("사용 가능한 아이디입니다");
+          isIdChecked = true;
         } else {
           alert("중복된 아이디입니다");
         }
@@ -24,10 +28,25 @@ $().ready(function () {
       success: function (response) {
         if (response.available) {
           alert("사용 가능한 이메일입니다");
+          isEmailChecked = true;
         } else {
           alert("중복된 이메일입니다");
         }
       },
     });
+  });
+
+  $("form").on("submit", function () {
+    if (!isIdChecked) {
+      alert("아이디 중복확인을 해주세요");
+      e.preventDefault;
+      return;
+    }
+
+    if (!isEmailChecked) {
+      alert("이메일 중복확인을 해주세요");
+      e.preventDefault;
+      return;
+    }
   });
 });
